ac1925974dd43856b8dd1694e0c932e75532f325
DA SILVA <davidfctt@gmail.com>
2022-02-03 21:09:03 +0100

    Fonctionnement de la tableView

---
+++ Event's/View/AttendeesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AttendeesTableViewCell.swift
+//  Event's
+//
+//  Created by David Da Silva on 03/02/2022.
+//
+
+import UIKit
+
+class AttendeesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
