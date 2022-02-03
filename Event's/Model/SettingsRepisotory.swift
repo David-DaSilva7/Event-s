d19f2e42f635e6254a2ec69f299261cb5cf29f79
DA SILVA <davidfctt@gmail.com>
2022-02-03 21:10:33 +0100

    Ajout de user default

--- Event's/Model/SettingsRepisotory.swift
+++ Event's/Model/SettingsRepisotory.swift
@@ -9,17 +9,42 @@ import Foundation
 
 class SettingsRepository {
     
-    private enum Keys {
-       static let attendees = "attendees"
+    func removeName(at index: Int) {
+        SettingsRepository.attendees.remove(at: index)
     }
     
-    static var attendees: String {
-       get {
-          return UserDefaults.standard.string(forKey: Keys.attendees) ?? "Nom de l'evenement"
-       }
-       set {
-          UserDefaults.standard.set(newValue, forKey: Keys.attendees)
-       }
+    private enum Keys {
+        static let nameEvents = "nameEvents"
+        static let numberDays = "numberDays"
+        static let attendees = "attendees"
+        
     }
-
+    
+    static var nameEvents: String {
+        get {
+            return UserDefaults.standard.string(forKey: Keys.nameEvents) ?? "Nom de l'evenement"
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: Keys.nameEvents)
+        }
+    }
+    
+    static var numberDays: String {
+        get {
+            return UserDefaults.standard.string(forKey: Keys.numberDays) ?? ""
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: Keys.numberDays)
+        }
+    }
+    
+    static var attendees: [String] {
+        get {
+            return UserDefaults.standard.object(forKey: Keys.attendees) as! [String]
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: Keys.attendees)
+        }
+    }
+    
 }
