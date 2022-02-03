119b9ac5d7ea04f7b666daf896d8fd6cc54bff7b
DA SILVA <davidfctt@gmail.com>
2022-02-03 21:10:00 +0100

    Fonctionnement de la tableView

--- Event's/Controller/AllInfoViewController.swift
+++ Event's/Controller/AllInfoViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 class AllInfoViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     
 
+    private let segueIdentifier = "segueToPersonInfo"
+    
     @IBOutlet weak var addPictureButton: UIButton!
     @IBOutlet weak var nameEventsLabel: UILabel!
     @IBOutlet weak var imageEvents: UIImageView!
+    @IBOutlet weak var numberDaysLabel: UILabel!
+    @IBOutlet weak var dayOrganizationTextView: UITextView!
+    @IBOutlet weak var attendeesTableView: UITableView!
+    
     
     @IBAction func buttonLibrary(_ sender: UIButton) {
 //        imagePicker.sourceType = .photoLibrary
@@ -26,6 +32,10 @@ class AllInfoViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
         {
             
         }
+    }
+    
+    @IBAction func dismissKeyborad(_ sender: UITapGestureRecognizer) {
+        dayOrganizationTextView.resignFirstResponder()
     }
     
     private func imagePickerControllerDidCancel(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -42,10 +52,14 @@ class AllInfoViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-//        let attendees = UserDefaults.standard.string(forKey: "attendees") ?? "Nom de l'evenement"
-        nameEventsLabel.text = SettingsRepository.attendees
+
+        nameEventsLabel.text = SettingsRepository.nameEvents
+        numberDaysLabel.text = SettingsRepository.numberDays 
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        attendeesTableView.reloadData()
     }
     
     
@@ -55,11 +69,25 @@ class AllInfoViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return InfoEvents.numberDaysArray.count
+        return InfoEvents.numberDaysArray.firstIndex(of: SettingsRepository.numberDays)! + 1
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return InfoEvents.numberDaysArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selection = InfoEvents.numberDaysArray[row]
+        print ("--")
+        print("ligne : ", row)
+        print ("colonne : ", component)
+        print ("Nombre de jour : ", selection)
+        
+//        if row == 0 {
+//            dayOrganizationTextView.text = "Acrobranche"
+//        }else if row == 1 {
+//            dayOrganizationTextView.text = "Piscine"
+//        }
     }
     
     
@@ -78,5 +106,55 @@ class AllInfoViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
 //            imagePicker.dismiss(animated: true, completion: nil)
 //        }
 //    }
+    var enterInfoViewController = EnterInfoViewController()
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == segueIdentifier {
+//            let personInfoVC = segue.destination as! PersonInfoViewController
+//        }
+//    }
+}
 
+// MARK: - TableView DataSource extension
+extension AllInfoViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    //        Nombres de sections
+    func numberOfSections(in tableView : UITableView) -> Int {
+        return 1
+    }
+    
+    //        Nombres de cellules
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return SettingsRepository.attendees.count
+    }
+    
+    //    Contenu dans la cellule
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "AttendeesCell",
+                                                            for: indexPath) as? AttendeesTableViewCell else {
+                                                                return UITableViewCell()
+        }
+
+        cell.configure(name: SettingsRepository.attendees[indexPath.row])
+        
+        return cell
+    }
+    
+    // MARK: - TableView Delegate extension
+    // Lorsqu'on appui sur une cellue
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        selectedRecipe = hits?[indexPath.row].recipe
+//        selectedRecipeImage = (tableView.cellForRow(at: indexPath) as! RecipeTableViewCell).picture.image
+//        performSegue(withIdentifier: segueIdentifier, sender: self)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+//            SettingsRepository.removeName(at: indexPath.row)
+//            enterInfoViewController.names.remove(at: indexPath.row)
+//            enterInfoViewController.removeName(indexName: indexPath.row)
+            SettingsRepository.attendees.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
 }
