963cfef01dccc335bcd0876a20d16d42355a51de
DA SILVA <davidfctt@gmail.com>
2022-04-19 11:18:10 +0200

    Integration du code pour changer description du theme

--- Event's/Controller/ChangeDescriptionThemeViewController.swift
+++ Event's/Controller/ChangeDescriptionThemeViewController.swift
@@ -8,23 +8,46 @@
 import UIKit
 
 class ChangeDescriptionThemeViewController: UIViewController {
-
     
+    // MARK: - Properties
+    var event: Event?
+    private var themes: [String: String] = [:]
+    var themeName = ""
+    weak var delegate: ThemeEnteredDelegate? = nil
+    
+    // MARK: - Outlets
     @IBOutlet weak var viewName: UIView!
     @IBOutlet weak var nameTheme: UILabel!
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var registerButton: UIButton!
     
+    // MARK: - Actions
+    @IBAction func registerButton(_ sender: Any) {
+        event?.themes[themeName] = descriptionTextView.text
+        print(event?.themes ?? [:])
+        delegate?.userDidEnteredTheme(keyTheme: themeName, valueTheme: descriptionTextView.text)
+    }
     
-    var themeName = ""
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        descriptionTextView.resignFirstResponder()
+    }
     
+    // MARK: - Functions
+    // Notifies the view controller that its view is about to be added to a view hierarchy.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        nameTheme.text = themeName
+        descriptionTextView.text = event?.themes[themeName]
+    }
+    
+    // Called after the controller's view is loaded into memory.
     override func viewDidLoad() {
         super.viewDidLoad()
-        nameTheme.text = themeName
         design()
     }
     
-    func design() {
+    // Design of the different elements
+    private func design() {
         viewName.layer.cornerRadius = 16
         registerButton.layer.cornerRadius = 16
         descriptionTextView.layer.borderColor = UIColor.black.cgColor
@@ -32,10 +55,6 @@ class ChangeDescriptionThemeViewController: UIViewController {
         descriptionTextView.layer.cornerRadius = 16
     }
     
-    @IBAction func registerButton(_ sender: Any) {
-    }
     
-    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
-        descriptionTextView.resignFirstResponder()
-    }
 }
+
