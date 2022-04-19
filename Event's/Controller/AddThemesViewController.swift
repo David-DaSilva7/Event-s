69e935b80dcbf0589999922331b70e54c731af81
DA SILVA <davidfctt@gmail.com>
2022-04-19 11:21:45 +0200

    Integration code pour ajouter un theme

--- Event's/Controller/AddThemesViewController.swift
+++ Event's/Controller/AddThemesViewController.swift
@@ -9,36 +9,56 @@ import UIKit
 
 class AddThemesViewController: UIViewController {
     
-    static var themes: [String] = []
+    // MARK: - Properties
+    var event: Event?
+    private var themes: [String: String] = [:]
+    weak var delegate: ThemeEnteredDelegate? = nil
     
+    // MARK: - Outlets
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var nameThemeTextField: UITextField!
+    @IBOutlet weak var register: UIButton!
     
+    // MARK: - Actions
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
         nameThemeTextField.resignFirstResponder()
         descriptionTextView.resignFirstResponder()
     }
     
     @IBAction func registerButton(_ sender: Any) {
-        if let nameTheme = nameThemeTextField.text, !nameTheme.isEmpty {
-            AddThemesViewController.themes.append(nameTheme)
-            let nameTheme = AddThemesViewController.themes
-        SettingsRepository.nameTheme = nameTheme
-            print("\(AddThemesViewController.themes)")
+        if var event = event {
+            //            EventsEntity.add(event.themes, in: event.id)
+            if let nameTheme = nameThemeTextField.text, !nameTheme.isEmpty {
+                themes = [nameTheme: descriptionTextView.text]
+                event.themes = themes
+                print(event.themes )
+                delegate?.userDidEnteredTheme(keyTheme: nameTheme, valueTheme: descriptionTextView.text)
+            }
         }
+        navigationController?.popViewController(animated: true)
     }
     
-    func design() {
+    // MARK: - Functions
+    // Called after the controller's view is loaded into memory.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        design()
+    }
+    
+    // Design of the different elements
+    private func design() {
         nameThemeTextField.layer.borderWidth = 0.5
         nameThemeTextField.layer.borderColor = UIColor.black.cgColor
         nameThemeTextField.layer.cornerRadius = 16
         descriptionTextView.layer.borderColor = UIColor.black.cgColor
         descriptionTextView.layer.borderWidth = 0.5
         descriptionTextView.layer.cornerRadius = 16
+        register.layer.cornerRadius = 16
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        design()
 }
+
+// MARK: - Protocols
+protocol ThemeEnteredDelegate: AnyObject {
+    // Protocol to adjust the table view with the new theme
+    func userDidEnteredTheme(keyTheme: String, valueTheme: String)
 }
